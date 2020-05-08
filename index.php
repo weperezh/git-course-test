e17274ad988ffbec31c62869a86c7c4365e22ecb
well <weperezh01@gmail.com>
2020-05-08 17:18:49 -0400

    mi segundo commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Well Technology</h1>
+    </header>
+    
+    <?php
+        $conexion=new mysqli("localhost","root","","internetclientes");
+        if($conexion->connect_errno){
+            echo "Falló la conexión " . $conexion->connect_errno;
+        }
+        //mysqli_set_charset($conexion, "utf8"); forma procedimental
+        $conexion->set_charset("utf8");
+        $sql="SELECT * FROM PRODUCTOS"
+        //$resulatdos=mysqli_query($conexion, $sql);
+        //$resultados=$conexion->query($sql);
+       // if($conexion->errno){
+        //    die($conexion->errno);
+      //  }
+       
+    ?>
+</body>
+</html>
